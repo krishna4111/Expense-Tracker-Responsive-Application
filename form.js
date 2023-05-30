8f401974622e5fbd5d9db7ea6ae7a78dcdfc8a4c
krishna4111 <krishnak4111@gmail.com>
2023-05-30 11:48:50 +0530

    second commit

---
+++ form.js
@@ -0,0 +1,32 @@
+console.log('js')
+async function submitEvent(e){
+ 
+    try{
+        e.preventDefault();
+        console.log(e.target.name.value)
+        const name=e.target.name.value;
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+        detail={
+            name,
+            email,
+            password
+        }
+        console.log(detail);
+     const response= await  axios.post('http://localhost:3000/user/signup',detail)
+     if(response.status===201){
+        window.location.href="../Login/login.html" //change the page on successful login
+     }
+     else{
+        throw new Error('Failed to login')
+     }
+     e.stopImmediatePropagation()
+      showExpenses(detail);
+    }
+    catch(err){
+        document.body.innerHTML += `<div style="color:red;">${err}</div>`
+    }
+}
+async function showExpenses(detail){
+
+}
